871931c5d1f20d191849251984cdc00a3da2778e
yan <yanhaibiao1991@gmail.com>
2023-06-28 12:41:11 +0800

    ..

--- index.js
+++ index.js
@@ -16,12 +16,16 @@ function rmOx(str) {
   return str.replace(/^0x/, "");
 }
 
-const signAuthMessage = async (resources) => {
-  // Replace this with you private key
+const getWallet = () => {
   const privKey =
     "03fc1e1d4a0db3ee7254ca41e6f1a4305d878f34c986b8ca8eab089653d68bf8";
   const privKeyBuffer = u8a.fromString(privKey, "base16");
   const wallet = new ethers.Wallet(privKeyBuffer);
+  return wallet;
+};
+
+const signAuthMessage = async (resources) => {
+  const wallet = getWallet();
 
   const domain = "localhost";
   const origin = "https://localhost/login";
@@ -116,14 +120,33 @@ async function test() {
   console.log("_symmetricKey", _symmetricKey);
 }
 
+export function projectMessageHash(user, uuids, requestor, nonce) {
+  const message = ethers.utils.defaultAbiCoder.encode(
+    ["address", "address", "uint256[]", "uint256"],
+    [user, requestor, uuids.sort(), nonce]
+  );
+  const hash = ethers.utils.keccak256(message);
+  return hash;
+}
+
 async function main() {
+  const symmetricKey = "";
+  const user = "0x2c11613DC6668fc000b31d8fDf9588a60053B6cc";
+  const requestor = "";
+  const uuids = [
+    "75682235775716889169053958985018289655377006682129274549968445093521439609384",
+    "93233475074686709017445900781235473934510624688497742929719674483975924837873",
+  ];
+  const msgHash = projectMessageHash(user, uuids, requestor, 0);
+  const wallet = getWallet();
+  const signature = await wallet.signMessage({
+    message: ethers.utils.arrayify(msgHash),
+  });
   const litNodeClient = new LitJsSdk.LitNodeClientNodeJs({
-    litNetwork: "serrano",
+    litNetwork: "jalapeno",
   });
   await litNodeClient.connect();
   const messageToEncrypt = "Lit is 🔥";
-
-  let authSig = await signAuthMessage();
 
   const evmContractConditions = [
     {
@@ -196,39 +219,6 @@ async function main() {
     },
   ];
 
-  // const { encryptedString, symmetricKey } = await LitJsSdk.encryptString(
-  //   messageToEncrypt
-  // );
-
-  // 2. Saving the Encrypted Content to the Lit Nodes
-  // // <Unit8Array> encryptedSymmetricKey
-  // const encryptedSymmetricKey = await litNodeClient.saveEncryptionKey({
-  //   evmContractConditions,
-  //   symmetricKey,
-  //   authSig,
-  //   chain: CHAIN,
-  //   permanent: 0,
-  // });
-
-  // const toDecrypt = LitJsSdk.uint8arrayToString(
-  //   encryptedSymmetricKey,
-  //   "base16"
-  // );
-
-  // const toDecrypt =
-  //   "954884e1dc75e31bcdc73d42b0dfd1e35ec68fcfd74373d343b1979a3c181caeb142d01b6db4bad18608b3c67fb0881941ee0ada4b8a80e433a951727859e0985b44a7be84040442795964cd78178d9bc1ab600cf3daed7c01ace37b5a864aabcf0d0174f0bc770197b1771e89709c534ae66b320c2fecd570f71542cb2be60a000000000000002031e1feefce84fcf0c774d8d7f390a9ae30a82802d80fd85e842b5dcc57ae50d87af5b96a4654f88c387c4e9b73e0ee86";
-
-  // console.log("toDecrypt", toDecrypt);
-
-  const user = "0x2c11613DC6668fc000b31d8fDf9588a60053B6cc";
-  const uuids = [
-    "75682235775716889169053958985018289655377006682129274549968445093521439609384",
-    "93233475074686709017445900781235473934510624688497742929719674483975924837873",
-  ];
-  // const requestor = "0x4c79B28335723E158b8F02b7E3191Aa570B2ED91";
-  // const signature =
-  //   "0x923b0161c68ead0e28efeff7e51da7ff31dedabd11ef827960bd5847b31c7987564444d61b4d907b6553d8f1cc45062360d64ea88ee58101fa8d0bcfa20c0a981c";
-
   const uuidStr = uuids
     .sort()
     .map((str) => {
@@ -238,11 +228,7 @@ async function main() {
 
   console.log("uuidStr", uuidStr);
 
-  // const signature =
-  //   "0x62c974e0ec01717d42e7b70bee9edf35db69acc01060524963ad6ec0f4bdc56b03e42bbe51022c7a78f47e7f311a0fb754f87e23e311a5f641dc63b9d0975df91b";
-  const signature =
-    "0xc019f6989de6652a450f227e5cc36c44d0617bba2c98cfda7055cc3e1699d35278c7c5c5eb12cac0be23519595817dff7c246982ec16a7f51165292e47dbaf0b1c";
-  authSig = await signAuthMessage([
+  const authSig = await signAuthMessage([
     `litParam:sceneIndex:${LitJsSdk.uint8arrayToString(
       LitJsSdk.uint8arrayFromString("1", "utf8"),
       "base64urlpad"
@@ -251,10 +237,6 @@ async function main() {
       LitJsSdk.uint8arrayFromString(rmOx(user), "utf8"),
       "base64urlpad"
     )}`,
-    // `litParam:requestor:${LitJsSdk.uint8arrayToString(
-    //   LitJsSdk.uint8arrayFromString(rmOx(requestor), "utf8"),
-    //   "base64urlpad"
-    // )}`,
     `litParam:uuids:${LitJsSdk.uint8arrayToString(
       LitJsSdk.uint8arrayFromString(`[${uuidStr}]`, "utf8"),
       "base64urlpad"
@@ -277,24 +259,10 @@ async function main() {
 
   const _symmetricKey = await litNodeClient.getEncryptionKey({
     evmContractConditions,
-    toDecrypt:
-      "0672507b482017ca5d0a7a4e0b6134b51b6c21d44cfd43c17abdb82bae8ec0659b7ee1ea63f7baee01456d203d951b1921393a1e7efbc80f970178df65802987388f92da29e62509992210605c4dd8012e6831197d90b77c348c3ae3236fca95158761fd2c96b752f81b8c3345adc245e9d913146bd3a6464295d6a65b029e2c000000000000002046f2ffff9b24547d7da2faf689d0f9059d22a80af4842f6f58397cf2b5d15d808286d02e25824d8b5912112e29fbcf87",
+    toDecrypt: symmetricKey,
     chain: CHAIN,
     authSig,
   });
-
-  let decryptedString;
-
-  // try {
-  //   decryptedString = await LitJsSdk.decryptString(
-  //     encryptedString,
-  //     _symmetricKey
-  //   );
-  // } catch (e) {
-  //   console.log(e);
-  // }
-
-  // console.warn("decryptedString:", decryptedString);
 }
 
 test();
